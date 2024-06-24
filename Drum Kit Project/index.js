ec2714c379eeebafd3f69a032979bd94d4ea3d25
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-06-24 20:29:33 +0600

    drum project completed

--- Drum Kit Project/index.js
+++ Drum Kit Project/index.js
@@ -6,42 +6,50 @@ function handleClick() {
   alert("I got clicked!");
 }
 
+// function to refer to a specific sound based on a specific key
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    default:
+      console.log(key);
+      break;
+  }
+}
+// Loop to handle clicks that correspond to a sound
 for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click", function () {
     let buttonHtml = this.innerHTML;
-    console.log(buttonHtml);
-
-    switch (buttonHtml) {
-      case "w":
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-        break;
-      case "a":
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-        break;
-      case "s":
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-        break;
-      case "d":
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-        break;
-      case "j":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-        break;
-      case "k":
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-        break;
-      case "l":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-        break;
-      default:
-        console.log(buttonHtml);
-    }
+    makeSound(buttonHtml);
   });
 }
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+});
