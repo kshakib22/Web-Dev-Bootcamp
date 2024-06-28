b534edb3bcb8a2e8d631045730ed1ce895347c9f
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-06-29 02:53:57 +0600

    Completed basic jQuery

--- Section 19 - jQuery/index.js
+++ Section 19 - jQuery/index.js
@@ -1,2 +1,15 @@
-$("h1").addClass("big-title");
-$("button").html("<em>Click<em>");
+// $("h1").addClass("bad-title");
+// $("button").html("<em>Click<em>");
+
+// $("button").click(function () {
+//   $("h1").removeClass("bad-title");
+//   $("h1").addClass("big-title");
+// });
+
+$("input").keydown(function (event) {
+  console.log(event.key);
+});
+
+$(document).keydown(function (event) {
+  $("h1").text(event.key);
+});
