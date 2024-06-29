0ff20815438bac7ad75e5462d7f64e71a6aed5f4
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-06-30 04:49:33 +0600

    Basic Functionalities added

--- Section 20 - Simon Game/game.js
+++ Section 20 - Simon Game/game.js
@@ -1,20 +1,39 @@
 let buttonColors = ["red", "green", "blue", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
 function nextSequence() {
   return Math.floor(Math.random() * 4);
 }
 
+function playSound(name) {
+  let path = "sounds/" + name + ".mp3";
+  let audio = new Audio(path);
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  let selector = "#" + currentColor;
+  $(selector).addClass("pressed");
+  setTimeout(function () {
+    $(selector).removeClass("pressed");
+  }, 113);
+}
 let randomChosenColor = buttonColors[nextSequence()];
 gamePattern.push(randomChosenColor);
 
 gamePattern;
+let colorID = gamePattern[0];
+colorID;
 
-// not corrected yet
-$(".button").click(function () {
-  $(this).animate(
-    {
-      opacity: 0.5,
-    },
-    500
-  );
+$("#" + colorID).click(function () {
+  playSound(colorID);
+  animatePress(colorID);
+
+  //   $(this).fadeOut(170).fadeIn(170);
+});
+
+$(".btn").click(function (event) {
+  userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
 });
