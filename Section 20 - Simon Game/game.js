0a0eff6f66534491467664a5fe5c4dd8bf80455d
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-07-01 04:53:33 +0600

    Only input queue remaining to be implemented

--- Section 20 - Simon Game/game.js
+++ Section 20 - Simon Game/game.js
@@ -1,13 +1,63 @@
 let buttonColors = ["red", "green", "blue", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+var level = 0;
+var activeGame = false;
 
-//   $(this).fadeOut(170).fadeIn(170);
+function onClick() {
+  $(".btn").click(function (event) {
+    userChosenColor = event.target.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    console.log("Length of game seq is " + gamePattern.length);
+    console.log("Length of user seq is " + userClickedPattern.length);
+    let index = userClickedPattern.length - 1;
+    checkAnswer(index);
+  });
+}
+
+function startOver() {
+  level = 0;
+  userClickedPattern = [];
+  gamePattern = [];
+}
+
+function checkAnswer(index) {
+  console.log(userClickedPattern);
+  console.log(gamePattern);
+  if (userClickedPattern[index] == gamePattern[index]) {
+    if (userClickedPattern.toString() == gamePattern.toString()) {
+      console.log("Correct");
+      setTimeout(nextSequence, 500);
+    }
+  } else {
+    console.log("Wrong");
+    gameOver();
+    startOver();
+    activeGame = false;
+  }
+}
+
+function gameOver() {
+  $("body").addClass("game-over");
+  $("h1").text("Game Over, press any key to restart");
+  let audio = new Audio("sounds/wrong.mp3");
+  setTimeout(function () {
+    $("body").removeClass("game-over");
+    audio.play();
+  }, 231);
+}
 
 function nextSequence() {
+  level += 1;
+  $("h1").text("Level " + level);
   var randomIndex = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomIndex];
   gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(200)
+    .fadeIn(200);
 }
 
 function playSound(name) {
@@ -24,10 +74,15 @@ function animatePress(currentColor) {
   }, 113);
 }
 
-$(".btn").click(function (event) {
-  userChosenColor = event.target.id;
-  userClickedPattern.push(userChosenColor);
-  playSound(userChosenColor);
-  animatePress(userChosenColor);
-  console.log(userClickedPattern);
-});
+function nextLevel() {
+  $(document).keydown(function (event) {
+    nextSequence();
+  });
+}
+
+function masterGame() {
+  nextLevel();
+  onClick();
+}
+
+masterGame();
