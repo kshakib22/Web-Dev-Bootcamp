1321ad93b4ed7c147068dbd34728eaf6addcd016
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-07-09 18:19:44 +0600

    Basic NPM 2.3

---
+++ Section 23 - Node Basics/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// var generateName = require("sillyname");
+import { randomSuperhero } from "superheroes";
+
+// var sillyName = generateName();
+// console.log(sillyName);
+// console.log(superheroes);
+
+const heroname = randomSuperhero();
+console.log(heroname);
