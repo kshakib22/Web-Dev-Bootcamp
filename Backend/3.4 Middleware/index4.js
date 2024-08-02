9fc53278a4d19ce7e7b3026136b1b11e1c1d13ae
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-08-02 15:11:09 +0600

    Update index4.js

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,8 +1,19 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function nameGen(req, res, next) {}
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
