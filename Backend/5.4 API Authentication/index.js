b4b2a519c570c36aec481b5e2a952402f03e4a59
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-08-25 14:14:37 +0600

    Update index.js

--- Backend/5.4 API Authentication/index.js
+++ Backend/5.4 API Authentication/index.js
@@ -17,16 +17,14 @@ app.get("/", (req, res) => {
 
 app.get("/noAuth", async (req, res) => {
   try {
-    const response = axios.get("https://secrets-api.appbrewery.com/random");
+    const response = await axios.get(API_URL + "random");
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
   } catch (error) {
-    res.status(404);
+    res.status(404).send(error.message);
   }
-  //TODO 2: Use axios to hit up the /random endpoint
-  //The data you get back should be sent to the ejs file as "content"
-  //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
 });
 
-app.get("/basicAuth", (req, res) => {
+app.get("/basicAuth", async (req, res) => {
   //TODO 3: Write your code here to hit up the /all endpoint
   //Specify that you only want the secrets from page 2
   //HINT: This is how you can use axios to do basic auth:
@@ -39,12 +37,28 @@ app.get("/basicAuth", (req, res) => {
       },
     });
   */
+  try {
+    const response = await axios.get(API_URL + "all?page=2", {
+      auth: { username: yourUsername, password: yourPassword },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-app.get("/apiKey", (req, res) => {
+app.get("/apiKey", async (req, res) => {
   //TODO 4: Write your code here to hit up the /filter endpoint
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
+  try {
+    const response = await axios.get(API_URL + "filter", {
+      headers: { score: "5", apiKey: yourAPIKey },
+    });
+    res.render("index.ejs", { content: JSON.stringify(response.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.get("/bearerToken", (req, res) => {
